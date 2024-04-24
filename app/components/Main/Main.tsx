f18cebc8b444c8111054c73c476971e52e189d34
izuchukwu-eric <onukwubeizu@gmail.com>
2024-04-24 14:44:14 +0100

    added dropdown

--- app/components/Main/Main.tsx
+++ app/components/Main/Main.tsx
@@ -1,17 +1,26 @@
 "use client"
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import { assets } from '../../assets/assets';
 import "./Main.css"
 import { Context } from '../../context/Context';
+import {
+    Collapsible,
+    CollapsibleContent,
+    CollapsibleTrigger,
+} from "@/components/ui/collapsible"
+import { ChevronsUpDown } from "lucide-react"
+  
 import Image from 'next/image';
+import { Button } from '@/components/ui/button';
 
 const Main = () => {
+    const [isOpen, setIsOpen] = useState<boolean>(true)
     const {onSent, recentPrompt, showResult, loading, resultData, setInput, input} = useContext(Context);
 
   return (
     <div className='main'>
         <div className="nav">
-            <p>Devin</p>
+            <p>WalletChat AI</p>
             <Image src={assets.user_icon} alt="" />
         </div>
         <div className="main-container">
@@ -19,28 +28,43 @@ const Main = () => {
             
                 ? 
                 <>
-                    <div className="greet">
-                        <p><span>Hello, Kevin.</span></p>
-                        <p>How can I help you today?</p>
-                    </div>
-                    <div className="cards">
-                        <div className="card">
-                            <p>Suggest beautiful places to see on an upcoming road trip</p>
-                            <Image src={assets.compass_icon} alt="" />
+                    <Collapsible
+                        open={isOpen}
+                        onOpenChange={setIsOpen}
+                        className="space-y-2"
+                    >
+                        <div className="flex greet items-center justify-between space-x-4 px-4">
+                            <span className="text-2xl font-semibold">
+                                What would you like to do?
+                            </span>
+                            <CollapsibleTrigger asChild>
+                            <Button variant="ghost" size="sm" className="w-9 p-0">
+                                <ChevronsUpDown className="h-4 w-4" color='#000' />
+                                <span className="sr-only">Toggle</span>
+                            </Button>
+                            </CollapsibleTrigger>
                         </div>
-                        <div className="card">
-                            <p>Briefly summarize this concept: urban planning</p>
-                            <Image src={assets.bulb_icon} alt="" />
-                        </div>
-                        <div className="card">
-                            <p>Brainstorm team bonding activities for our work retreat</p>
-                            <Image src={assets.message_icon} alt="" />
-                        </div>
-                        <div className="card">
-                            <p>Improve the readability of the following code</p>
-                            <Image src={assets.code_icon} alt="" />
-                        </div>
-                    </div>
+                        <CollapsibleContent>
+                            <div className="cards">
+                                <div className="card">
+                                    <p>Suggest beautiful places to see on an upcoming road trip</p>
+                                    <Image src={assets.compass_icon} alt="" />
+                                </div>
+                                <div className="card">
+                                    <p>Briefly summarize this concept: urban planning</p>
+                                    <Image src={assets.bulb_icon} alt="" />
+                                </div>
+                                <div className="card">
+                                    <p>Brainstorm team bonding activities for our work retreat</p>
+                                    <Image src={assets.message_icon} alt="" />
+                                </div>
+                                <div className="card">
+                                    <p>Improve the readability of the following code</p>
+                                    <Image src={assets.code_icon} alt="" />
+                                </div>
+                            </div>
+                        </CollapsibleContent>
+                    </Collapsible>
                 </>
                 :
                 <div className='result'>
@@ -74,7 +98,7 @@ const Main = () => {
                     </div>
                 </div>
                 <p className="bottom-info">
-                    Devin may display inaccurate info, including about people, so double-check its responses. Your privacy and Gemini Apps.
+                    WalletChat AI may display inaccurate info, including about people, so double-check its responses. Your privacy and WalletChat Apps.
                 </p>
             </div>
         </div>
